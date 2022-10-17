3b78fb2856d9df1cfce265f41a0d7459330440da
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-17 11:05:16 +0300

    Второе решение  задачи 9

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -12,15 +12,26 @@ int secondDigit = number % 10; // 78 % 10 = 8
 // else Console.WriteLine($"Наибольшая цифра числа равна {secondDigit}");
 
 // Условный оператор
-int result = default;
-if(firstDigit > secondDigit) result = firstDigit;
-else result = secondDigit;
-Console.WriteLine($"Наибольшая цифра числа равна {result}");
+// int result = default;
+// if(firstDigit > secondDigit) result = firstDigit;
+// else result = secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа равна {result}");
 // Тернарный оператор 1
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа равна {maxDigit}");
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа равна {maxDigit}");
 // Тернарный оператор 2
+// Console.Write($"Наибольшая цифра числа {number} равна ");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10; // 78 / 10 = 7
+    int secondDigit = number % 10; // 78 % 10 = 8
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
 Console.Write($"Наибольшая цифра числа {number} равна ");
 Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
-
-
