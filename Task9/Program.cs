84afaa44523efc0c24b5688a4e16181e122c4907
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-17 10:52:04 +0300

    Перваое решение задачи

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -2,5 +2,25 @@
 // 1. выводит случайное число из отрезка (10,99)
 //2. показывает наибольшую цифру числа
 
-int number = 88; // new Ramdom().Next(10, 100)
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+int firstDigit = number / 10; // 78 / 10 = 7
+int secondDigit = number % 10; // 78 % 10 = 8
+
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа равна {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа равна {secondDigit}");
+
+// Условный оператор
+int result = default;
+if(firstDigit > secondDigit) result = firstDigit;
+else result = secondDigit;
+Console.WriteLine($"Наибольшая цифра числа равна {result}");
+// Тернарный оператор 1
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа равна {maxDigit}");
+// Тернарный оператор 2
+Console.Write($"Наибольшая цифра числа {number} равна ");
+Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
 
